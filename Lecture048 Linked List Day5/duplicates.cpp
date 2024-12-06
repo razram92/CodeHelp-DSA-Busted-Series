9f1e39a4ac002d31b0da689f5d68f8ea05b7f189
razram92 <rohitatcollege22@gmail.com>
2024-12-06 23:47:43 +0530

    Update duplicates.cpp
    contains code of unsorted link list using map

--- Lecture048 Linked List Day5/duplicates.cpp
+++ Lecture048 Linked List Day5/duplicates.cpp
@@ -41,3 +41,27 @@ Node * uniqueSortedList(Node * head) {
     
     return head; 
 }
+
+
+
+Remove unsorted LL using map
+Node *removeDuplicates(Node *head)
+{    
+    Node* curr = head;
+    Node* prev = NULL;
+    unordered_map<int, bool> visited;
+    
+    while(curr != NULL){
+        if(!visited[curr->data]){
+            visited[curr->data] = true;
+            prev = curr;
+            curr = curr -> next;
+        }
+        else{
+            prev -> next = curr -> next;
+            delete curr;
+        }
+        curr = prev -> next;
+    }
+    return head;
+}
